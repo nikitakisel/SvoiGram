167661b18f3c08a141559c4da53a343a897db26c
Никита Киселев <nikitakiselev@MacBook-Air-Nikita.local>
2025-03-04 16:39:38 +0300

    Prototype of posts showing

--- SvoiGram/News/NewsTableViewCell.swift
+++ SvoiGram/News/NewsTableViewCell.swift
@@ -10,19 +10,36 @@ import UIKit
 
 class NewsTableViewCell: UITableViewCell {
     
+    private var postId: Int
     @IBOutlet private weak var postTitle: UILabel!
     @IBOutlet private weak var postPlace: UILabel!
     @IBOutlet private weak var postImage: UIImageView!
     @IBOutlet private weak var postAuthor: UILabel!
     @IBOutlet private weak var postDescription: UILabel!
     
-    func configure(postTitle: String, postPlace: String, postImage: String, postAuthor: String, postDescription: String) {
+    func displayBase64Image(imageData: Data, imageView: UIImageView) {
+        if let image = UIImage(data: imageData) {
+            imageView.image = image // imageView - ваш UIImageView
+        } else {
+            // Обработка ошибки:
+            print("Ошибка: Не удалось создать изображение из предоставленных данных.")
+            // Например, можно установить placeholder image:
+            imageView.image = UIImage(named: "placeholderImage")
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func configure(postId: Int, postTitle: String, postPlace: String, postImage: Data, postAuthor: String, postDescription: String) {
+        self.postId = postId
         self.postTitle.text = postTitle
         self.postPlace.text = postPlace
 //        self.postImage.cgImage = postImage
         self.postAuthor.text = postAuthor
         self.postDescription.text = postDescription
-        
+        displayBase64Image(imageData: postImage, imageView: self.postImage)
     }
-    
 }
